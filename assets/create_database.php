8b331194b6e8afa060ef54a5794e6f3c16c67c0e
Viduranga Wijesooriya <vpowerrc@gmail.com>
2014-07-12 13:40:02 +0530

    updated php cgi
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- assets/create_database.php
+++ assets/create_database.php
@@ -1,142 +1,11 @@
 <?php
 
-
-
 // MySQL host
 $mysql_host = 'localhost';
 // MySQL username
 $mysql_username = 'root';
 // MySQL password
 $mysql_password = '';
-// Database name
-$mysql_database = "vesuvius";
-// Name of the sql dump file
-$sql_filename = '../vesuvius-master/vesuvius/backups/vesuviusStarterDb_v092.sql';
-
-
-
-// Connect to MySQL
-$link = mysql_connect($mysql_host, $mysql_username, $mysql_password );
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-
-// Make my_db the current database
-$db_selected = mysql_select_db($mysql_database, $link);
-
-if (!$db_selected) {
-  // If we couldn't, then it either doesn't exist, or we can't see it.
-  $sql = 'CREATE DATABASE '.$mysql_database;
-
-  if (mysql_query($sql, $link)) {
-      echo "Database '.$mysql_database.' created successfully\n";
-  } else {
-      echo 'Error creating database: ' . mysql_error() . "\n";
-  }
-}
-
-mysql_close($link);
-
-
-/*
-
-// Connect to MySQL server
-mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
-// Select database
-mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
-
-// Temporary variable, used to store current query
-$templine = '';
-// Read in entire file
-$lines = file($filename);
-// Loop through each line
-foreach ($lines as $line)
-{
-// Skip it if it's a comment
-if (substr($line, 0, 2) == '--' || $line == '')
-    continue;
-
-// Add this line to the current segment
-$templine .= $line;
-// If it has a semicolon at the end, it's the end of the query
-if (substr(trim($line), -1, 1) == ';')
-{
-    // Perform the query
-    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
-    // Reset temp variable to empty
-    $templine = '';
-}
-}
- echo "Tables imported successfully";
-
-*/
-
-
-
-function remove_comments(&$output)
-{
-   $lines = explode("\n", $output);
-   $output = "";
-
-   // try to keep mem. use down
-   $linecount = count($lines);
-
-   $in_comment = false;
-   for($i = 0; $i < $linecount; $i++)
-   {
-      if( preg_match("/^\/\*/", preg_quote($lines[$i])) )
-      {
-         $in_comment = true;
-      }
-
-      if( !$in_comment )
-      {
-         $output .= $lines[$i] . "\n";
-      }
-
-      if( preg_match("/\*\/$/", preg_quote($lines[$i])) )
-      {
-         $in_comment = false;
-      }
-   }
-
-   unset($lines);
-   return $output;
-}
-
-//
-// remove_remarks will strip the sql comment lines out of an uploaded sql file
-//
-function remove_remarks($sql)
-{
-   $lines = explode("\n", $sql);
-
-   // try to keep mem. use down
-   $sql = "";
-
-   $linecount = count($lines);
-   $output = "";
-
-   for ($i = 0; $i < $linecount; $i++)
-   {
-      if (($i != ($linecount - 1)) || (strlen($lines[$i]) > 0))
-      {
-         if (isset($lines[$i][0]) && $lines[$i][0] != "#")
-         {
-            $output .= $lines[$i] . "\n";
-         }
-         else
-         {
-            $output .= "\n";
-         }
-         // Trading a bit of speed for lower mem. use here.
-         $lines[$i] = "";
-      }
-   }
-
-   return $output;
-
-}
 
 //
 // split_sql_file will split an uploaded sql file into single sql statements.
@@ -230,20 +99,63 @@ function split_sql_file($sql, $delimiter)
    return $output;
 }
 
-$sql_query = @fread(@fopen($sql_filename, 'r'), @filesize($sql_filename)) or die('problem ');
-$sql_query = remove_remarks($sql_query);
-$sql_query = split_sql_file($sql_query, ';');
+// import sql file
+function import_sql($database,$sql_file){
+
+   global $link;
+
+   $raw_sql = @fread(@fopen($sql_file, 'r'), @filesize($sql_file)) or die('problem ');
+
+   mysql_select_db($database, $link);
+
+   $raw_sql_array = preg_split('/DELIMITER\s(\S+)\s/', $raw_sql, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+   $delimiter = ";";
+   if(count($raw_sql_array) > 0)
+   {
+      $sql_query = split_sql_file($raw_sql_array[0], $delimiter);
+   }
+   else{
+      $sql_query = split_sql_file($raw_sql, $delimiter);
+   }
 
 
-mysql_connect($mysql_host,$mysql_username,$mysql_password) or die('error connection');
-mysql_select_db($mysql_database) or die('error database selection');
+   foreach($sql_query as $sql){  
+      mysql_query($sql) or die(mysql_error());
+   }
 
-$i=1;
-foreach($sql_query as $sql){
-echo $i++;
-echo "
-";
-mysql_query($sql) or die($sql);
+   for ($j = 1; $j < count($raw_sql_array); $j=$j+2) {
+
+      $delimiter = $raw_sql_array[$j]; 
+      $sql_query = split_sql_file($raw_sql_array[$j+1], $delimiter);
+
+      foreach($sql_query as $sql){  
+         mysql_query($sql) or die(mysql_error());  
+      }
+   }
+
 }
 
+// create database if not exists
+function create_database($database_name){
+   mysql_query('CREATE DATABASE IF NOT EXISTS '.$database_name) or die(mysql_error());
+}
+
+// connect to MySQL
+$link = mysql_connect($mysql_host, $mysql_username, $mysql_password );
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+
+create_database("vesuvius");
+
+$vesuvius_sql_file = '../vesuvius-master/vesuvius/backups/vesuviusStarterDb_v092.sql';
+import_sql("vesuvius",$vesuvius_sql_file);
+
+import_sql("mysql","timezones.sql");
+
+mysql_close($link);
+
+echo "database imported successfully"
 ?>
